b82f87716b7a265ff523502ab9a6ce4822259e7c
mockyli <franklin_li@qq.com>
2022-11-24 16:25:01 +0800

    + Task 基本应用

---
+++ src/CSharpPractic/CSharpPractic.ConsoleMain/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpPractic.ConsoleMain
+{
+
+    
+
+    public class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+             Task.Run(() => 
+             {
+                 for (int i = 0; i < 1000; i+=2)
+                 {
+                     Console.WriteLine("current val : " + i.ToString()  );
+                     Thread.Sleep(20);
+                 }
+             });
+
+            for (int i = 1; i < 1000; i+=2)
+            {
+                Console.WriteLine("Main val : " + i.ToString());
+                Thread.Sleep(20);
+            }
+
+
+            Console.Read(); 
+        }
+    }
+}
